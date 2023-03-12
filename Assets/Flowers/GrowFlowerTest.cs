3daf20999f5f8f88f4e1c140228bdf919a19c551
ainee-jane <an-ni-ka@gmx.de>
2023-03-12 12:20:15 +0100

    added flower and grow script

---
+++ Assets/Flowers/GrowFlowerTest.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class GrowFlowerTest : MonoBehaviour
+{
+    public float growthRate = 0.01f; // Wachstumsrate der Blume
+    public float maxGrowth = 1f; // Maximale Größe der Blume
+    public KeyCode growthKey = KeyCode.Space; // Taste, um das Wachstum auszulösen
+
+    private float currentGrowth = -0.35f; // Aktuelle Größe der Blume
+    private bool hasStartedGrowing = false; // Hat die Blume schon mit dem Wachsen begonnen?
+
+    void Update()
+    {
+        // Wenn die Wachtums-Taste gedrückt wird und das Wachstum noch nicht begonnen hat, beginne mit dem Wachsen
+        if (Input.GetKeyDown(growthKey) && !hasStartedGrowing)
+        {
+            hasStartedGrowing = true;
+            StartCoroutine(IncreaseGrowth());
+        }
+    }
+
+    // Schrittweise erhöhe das Wachstum der Blume, bis es den maximalen Wert erreicht hat
+    IEnumerator IncreaseGrowth()
+    {
+        while (currentGrowth < maxGrowth)
+        {
+            currentGrowth += growthRate;
+            currentGrowth = Mathf.Clamp(currentGrowth, -0.35f, maxGrowth);
+            UpdateGrowth();
+
+            yield return new WaitForSeconds(0.1f); // Warte 0.1 Sekunden, bevor das Wachstum erneut erhöht wird
+        }
+    }
+
+    // Aktualisiere die Größe der Blume im Material
+    void UpdateGrowth()
+    {
+        
+        Material material = GetComponent<Renderer>().material;
+        material.SetFloat("Grow_", currentGrowth);
+        Debug.Log("Current Grow_ value: " + material.GetFloat("Grow_"));
+
+    }
+}

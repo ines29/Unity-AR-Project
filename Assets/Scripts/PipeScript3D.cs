f5d9bd4e6853c15a53340b7d56fc04cec8eb8c19
i.trautmannsheimer <i.trautmannsheimer@campus.lmu.de>
2023-03-13 14:27:22 +0100

    Setup 3D Puzzle

---
+++ Assets/Scripts/PipeScript3D.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeScript3D : MonoBehaviour
+{
+    float[] rotations = { 0, 90, 180, 270 };
+    public float[] correctRotation;
+    [SerializeField]
+    bool isPlaced = false;
+
+    int PossibleRots = 1;
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager3D").GetComponent<GameManager>();
+    }
+    private void Start()
+    {
+        PossibleRots = correctRotation.Length;
+        int rand = Random.Range(0, rotations.Length);
+        Debug.Log(Quaternion.identity.x);
+        transform.Rotate(new Vector3(rotations[rand],0,0));
+        Debug.Log(Quaternion.identity.x);
+        Debug.Log(transform.eulerAngles.x.ToString()+" random: "+rotations[rand].
+            ToString());
+        if (PossibleRots > 1)
+        {
+
+            if (transform.eulerAngles.x == correctRotation[0] || transform.eulerAngles.x == correctRotation[1])
+            {
+                isPlaced = true;
+                gameManager.correctMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.x == correctRotation[0])
+            {
+                isPlaced = true;
+                gameManager.correctMove();
+            }
+        }
+
+    }
+
+
+    private void OnMouseDown()
+    {
+      //  Debug.Log("Before: " + (transform.eulerAngles.x).ToString());
+
+        transform.Rotate(new Vector3(-90, 0, 0));
+        if (PossibleRots > 1)
+        {
+ 
+            if ((transform.eulerAngles.x == correctRotation[0] || transform.eulerAngles.x == correctRotation[1]) && isPlaced == false)
+            {
+                isPlaced = true;
+                gameManager.correctMove();
+            }
+            else if (isPlaced == true)
+            {
+                isPlaced = false;
+                gameManager.wrongMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.x == correctRotation[0] && isPlaced == false)
+            {
+                isPlaced = true;
+                gameManager.correctMove();
+            }
+            else if (isPlaced == true)
+            {
+                isPlaced = false;
+                gameManager.wrongMove();
+            }
+        }
+        Debug.Log(isPlaced.ToString() + transform.eulerAngles.x);
+       // Debug.Log(isPlaced.ToString() + (transform.eulerAngles.x).ToString());
+
+    }
+    public void ButtonPressed()
+    {
+        Debug.Log("Button Pressed");
+        transform.Rotate(new Vector3(-90, 0, 0));
+        if (PossibleRots > 1)
+        {
+            if ((transform.eulerAngles.x == correctRotation[0] || transform.eulerAngles.x == correctRotation[1]) && isPlaced == false)
+            {
+                isPlaced = true;
+                gameManager.correctMove();
+            }
+            else if (isPlaced == true)
+            {
+                isPlaced = false;
+                gameManager.wrongMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.x == correctRotation[0] && isPlaced == false)
+            {
+                isPlaced = true;
+                gameManager.correctMove();
+            }
+            else if (isPlaced == true)
+            {
+                isPlaced = false;
+                gameManager.wrongMove();
+            }
+        }
+
+    }
+}

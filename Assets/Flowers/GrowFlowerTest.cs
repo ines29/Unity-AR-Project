c45c2f9282634f38c8eedd8c4f7f02e7a806ead7
ainee-jane <an-ni-ka@gmx.de>
2023-03-13 16:43:11 +0100

    numbers added

--- Assets/Flowers/GrowFlowerTest.cs
+++ Assets/Flowers/GrowFlowerTest.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class GrowFlowerTest : MonoBehaviour
 {
@@ -9,7 +10,9 @@ public class GrowFlowerTest : MonoBehaviour
 
     private float currentGrowth = -0.35f; // Aktuelle Größe der Blume
     private bool hasStartedGrowing = false; // Hat die Blume schon mit dem Wachsen begonnen?
+    public GameObject textObject;
 
+    //public TextMeshProUGUI flowerText;  // Drag & Drop Text Objekt auf dieses Feld im Inspector
     void Update()
     {
         // Wenn die Wachtums-Taste gedrückt wird und das Wachstum noch nicht begonnen hat, beginne mit dem Wachsen
@@ -33,6 +36,8 @@ public class GrowFlowerTest : MonoBehaviour
         }
     }
 
+
+
     // Aktualisiere die Größe der Blume im Material
     void UpdateGrowth()
     {
@@ -41,5 +46,19 @@ public class GrowFlowerTest : MonoBehaviour
         material.SetFloat("Grow_", currentGrowth);
         Debug.Log("Current Grow_ value: " + material.GetFloat("Grow_"));
 
+        // Wenn das Wachstum die maximale Größe erreicht hat, zeige den Text auf der Blume an
+        if (currentGrowth == maxGrowth)
+        {
+            textObject.SetActive(true); // Aktiviert das Text-GameObjekt, wenn die Blume vollständig gewachsen ist
+        }
+        else
+        {
+            textObject.SetActive(false); // Deaktiviert das Text-GameObjekt, wenn die Blume noch nicht vollständig gewachsen ist
+        }
+
     }
 }
+
+//Add Sound Effect?
+
+

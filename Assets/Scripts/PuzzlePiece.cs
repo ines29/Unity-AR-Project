abc319f2cd8c67044cfb56f2eee1b671e48ad9e3
i.trautmannsheimer <i.trautmannsheimer@campus.lmu.de>
2023-03-14 10:49:54 +0100

    Snapping

--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -1,3 +1,5 @@
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +10,7 @@ public class PuzzlePiece : MonoBehaviour
     public bool inRightPosition; // gibt an, ob das Puzzleteil bereits an der korrekten Position ist
     public bool selected; // gibt an, ob das Puzzleteil gerade ausgewählt ist
     public GameObject placeholderPrefab; // das Prefab des Platzhalter-GameObjects
+    public GameObject script;
 
     private List<Transform> snaps = new List<Transform>(); // Liste der Zielpositionen
 
@@ -23,37 +26,27 @@ public class PuzzlePiece : MonoBehaviour
         GameObject placeholder = Instantiate(placeholderPrefab, rightPosition, Quaternion.identity);
         placeholder.name = gameObject.name + " Placeholder";
 
-        // Fülle die Liste der Zielpositionen mit den korrekten Positionen
-        GameObject[] snapPositions = GameObject.FindGameObjectsWithTag("SnapPosition");
-        foreach (GameObject snapPosition in snapPositions)
-        {
-            snaps.Add(snapPosition.GetComponent<Transform>());
-        }
-        Debug.Log("Zielpositionen: " + GameObject.FindGameObjectsWithTag("SnapPosition"));
+       // Debug.Log("Zielpositionen: " + GameObject.FindGameObjectsWithTag("SnapPosition"));
     }
 
     void Update()
     {
-        // Aktualisiere das Puzzleteil nur, wenn es nicht ausgewählt ist
-        if (!selected)
+       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name+ gameObject.name);
+        if (other.name == gameObject.name+" Placeholder")
         {
-            // Prüfe, ob das Puzzleteil an der richtigen Position ist
-            // Iteriere über die Liste der Zielpositionen
-            foreach (Transform snap in snaps)
-            {
+            Debug.Log("Match");
+         
                 // Prüfe, ob das Puzzleteil sich innerhalb des Radius von der aktuellen Zielposition befindet
-                if (Vector3.Distance(snap.position, transform.position) < 0.5f)
-                {
-                  
-                    transform.position = snap.position;  // Bewege das Puzzleteil auf die Zielposition
+
+                    transform.position = rightPosition;  // Bewege das Puzzleteil auf die Zielposition
                     inRightPosition = true;  // Markiere das Puzzleteil als an der korrekten Position
-                    break;
-                }
-                else
-                {
-                    inRightPosition = false;  //  Puzzleteil ist nicht an der korrekten Position
-                }
-            }
+                     GetComponent<NearInteractionGrabbable>().enabled = false;
+                     GetComponent<ObjectManipulator>().enabled = false;
         }
+
     }
 }

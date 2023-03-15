60310d117fe28ace513d5bc346aeae79b648443f
i.trautmannsheimer <i.trautmannsheimer@campus.lmu.de>
2023-03-15 17:30:07 +0100

    Added Win mechanics for water puzzle

---
+++ Assets/Scripts/PlayerProgress.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerProgress
+{
+    public static bool wonCubes { get; set; }
+    public static bool wonWaterPuzzle { get; set; }
+    public static bool wonChess { get; set; }
+    public static bool wonJigsaw { get; set; }
+    public static bool wonSchiebepuzzle { get; set; }
+    public static bool wonLabyrinth { get; set; }
+
+
+}

cc9c65121855119df117abdfcf1530d9e947bc38
i.trautmannsheimer <i.trautmannsheimer@campus.lmu.de>
2023-03-15 10:53:47 +0100

    Bugfix winning condition

--- Assets/Scripts/PipeScript3D.cs
+++ Assets/Scripts/PipeScript3D.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PipeScript3D : MonoBehaviour
 {
     float[] rotations = { 0, 90, 180, 270 };
-    public float[] correctRotation;
-    [SerializeField]
     bool isPlaced = false;
     [SerializeField]
     int connections = 0;
@@ -20,29 +18,10 @@ public class PipeScript3D : MonoBehaviour
     }
     private void Start()
     {
-        PossibleRots = correctRotation.Length;
-        int rand = Random.Range(0, rotations.Length);
-        //Debug.Log(Quaternion.identity.x);
-        transform.Rotate(new Vector3(rotations[rand],0,0));
-        //Debug.Log(Quaternion.identity.x);
-        //Debug.Log(transform.eulerAngles.x.ToString()+" random: "+rotations[rand].ToString());
-        if (PossibleRots > 1)
-        {
 
-            if (transform.eulerAngles.x == correctRotation[0] || transform.eulerAngles.x == correctRotation[1])
-            {
-                isPlaced = true;
-                gameManager.correctMove();
-            }
-        }
-        else
-        {
-            if (transform.eulerAngles.x == correctRotation[0])
-            {
-                isPlaced = true;
-                gameManager.correctMove();
-            }
-        }
+        int rand = Random.Range(0, rotations.Length);
+        transform.Rotate(new Vector3(rotations[rand],0,0));
+       
 
     }
 

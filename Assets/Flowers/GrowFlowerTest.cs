69cd138bcad3c0375797aa8f26b33eb4724c031d
ainee-jane <an-ni-ka@gmx.de>
2023-03-16 17:54:44 +0100

    flower numbers updated

--- Assets/Flowers/GrowFlowerTest.cs
+++ Assets/Flowers/GrowFlowerTest.cs
@@ -11,17 +11,28 @@ public class GrowFlowerTest : MonoBehaviour
     private float currentGrowth = -0.35f; // Aktuelle Größe der Blume
     private bool hasStartedGrowing = false; // Hat die Blume schon mit dem Wachsen begonnen?
     public GameObject textObject;
+    public GameObject textObject1;
+    public GameObject textObject2;
+    public GameObject textObject3;
 
     //public TextMeshProUGUI flowerText;  // Drag & Drop Text Objekt auf dieses Feld im Inspector
-    void Update()
+    /* void Update()
+     {
+         // Wenn die Wachtums-Taste gedrückt wird und das Wachstum noch nicht begonnen hat, beginne mit dem Wachsen
+         if (Input.GetKeyDown(growthKey) && !hasStartedGrowing)
+         {
+             hasStartedGrowing = true;
+             StartCoroutine(IncreaseGrowth());
+         }
+     }
+    */
+
+    public void growButton()
     {
-        // Wenn die Wachtums-Taste gedrückt wird und das Wachstum noch nicht begonnen hat, beginne mit dem Wachsen
-        if (Input.GetKeyDown(growthKey) && !hasStartedGrowing)
-        {
-            hasStartedGrowing = true;
-            StartCoroutine(IncreaseGrowth());
-        }
+        hasStartedGrowing = true;
+        StartCoroutine(IncreaseGrowth());
     }
+
 
     // Schrittweise erhöhe das Wachstum der Blume, bis es den maximalen Wert erreicht hat
     IEnumerator IncreaseGrowth()
@@ -45,18 +56,32 @@ public class GrowFlowerTest : MonoBehaviour
         Material material = GetComponent<Renderer>().material;
         material.SetFloat("Grow_", currentGrowth);
         Debug.Log("Current Grow_ value: " + material.GetFloat("Grow_"));
-
         // Wenn das Wachstum die maximale Größe erreicht hat, zeige den Text auf der Blume an
         if (currentGrowth == maxGrowth)
         {
-            textObject.SetActive(true); // Aktiviert das Text-GameObjekt, wenn die Blume vollständig gewachsen ist
+            StartCoroutine(passwordWait());  
         }
         else
         {
             textObject.SetActive(false); // Deaktiviert das Text-GameObjekt, wenn die Blume noch nicht vollständig gewachsen ist
+            textObject1.SetActive(false);
+            textObject2.SetActive(false);
+            textObject3.SetActive(false);
         }
 
     }
+
+    IEnumerator passwordWait()
+    {
+        textObject.SetActive(true);
+        yield return new WaitForSeconds(1); // Warte 0.5 Sekunden, bevor das Wachstum erneut erhöht wird
+        textObject1.SetActive(true);
+        yield return new WaitForSeconds(1); // Warte 0.5 Sekunden, bevor das Wachstum erneut erhöht wird
+        textObject2.SetActive(true);
+        yield return new WaitForSeconds(1); // Warte 0.5 Sekunden, bevor das Wachstum erneut erhöht wird
+        textObject3.SetActive(true);
+    }
+
 }
 
 //Add Sound Effect?
